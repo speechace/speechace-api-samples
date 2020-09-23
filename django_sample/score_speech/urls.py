c7d3ac5f8023319e15848e358e033cb031a8a345
Vitalii Vdovenko <vitalii.vdoveno@yanpix.com>
2020-09-23 20:57:12 +0300

    ISP-857 create api method for new free speech mode aproach

--- django_sample/score_speech/urls.py
+++ django_sample/score_speech/urls.py
@@ -21,10 +21,11 @@
 # IN THE SOFTWARE
 from django.urls import re_path
 
-from .views import CsrfTemplateView, scoring_text_speech
+from .views import CsrfTemplateView, scoring_text_speech, scoring_free_speech
 
 app_name = 'score_speech'
 urlpatterns = [
     re_path(r'^$', CsrfTemplateView.as_view(template_name='index.html'), name='index'),
     re_path(r'^score-text-speech/$', scoring_text_speech, name='score_text_speech'),
+    re_path(r'^score-free-speech/$', scoring_free_speech, name='score_free_speech'),
 ]
